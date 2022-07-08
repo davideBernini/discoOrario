6de9901140c80b402b8fd56d6831c739e7c5e29d
davide_bernini <dbernini@studenti.uninsubria.it>
2022-07-08 03:47:04 +0200

    “Initial commit of project files”

---
+++ app/src/main/java/it/uninsubria/primaapp/discoorario/MainActivity.kt
@@ -0,0 +1,29 @@
+package it.uninsubria.primaapp.discoorario
+
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottone2.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                mostra_tempo.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(
+                this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), true
+            ).show()
+
+        }
+
+    }
+}
